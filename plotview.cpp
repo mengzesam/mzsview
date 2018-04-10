5f475e3781c31aa48f28811f04f6e263fbc547df
mengzesam <mengzesam@126.com>
2018-04-10 21:56:15 +0800

    Add files via upload

--- plotview.cpp
+++ plotview.cpp
@@ -1,4 +1,6 @@
 #include "plotview.h"
+#include <QDebug>
+#include <QTime>
 
 PlotView::PlotView(QWidget *parent) :
       QChartView(parent),
@@ -17,7 +19,11 @@ PlotView::PlotView(QWidget *parent) :
       m_pickedIndex(0),
       m_prePickedIndex(0),
       m_current_lefttime(0),
-      m_time_span(7200000)
+      m_time_span(7200000),
+      m_leftpoint_index(0),
+      m_rightpoint_index(240),
+      m_starttime(0),
+      m_endtime(6*7200000)
 {    
     for(int i=0;i<mzsview::ROWS;i++){
         m_values_min[i]=mzsview::DEFAULT_MINS[i];
@@ -94,36 +100,61 @@ void PlotView::emptyChart()
     m_isloadchart=false;
 }
 
-int PlotView::setAllLineData()
+int PlotView::setLineData(int flag,int line_No, int field_index)//flag 0:called by loadChart,and set all line
+                                      //flag 1,2,3:called by nexttime\pretime\settimespan
+                                      //flag 4:set single line
+                                      //flag 0-3:line_No field_index is not used
 {
     if(!m_filevalid)
         return -1;
-
     QFile datafile(m_datafile);
     if (!datafile.open(QIODevice::ReadOnly | QIODevice::Text)) {
          return -1;
      }
     QTextStream stream(&datafile);
-    QString line = (stream.readLine()).trimmed();
-    QStringList values;
-    if(line.isEmpty()){
-        datafile.close();
-        return -1;
-    }
-    values = line.split(mzsview::DATA_DELIMITER, QString::SkipEmptyParts);
-    if(values.size()<2){
-        datafile.close();
-        return -1;
-    }
-    emptyChart();//empty data for every line series
-    m_valid_lines=mzsview::ROWS<values.size()-1?mzsview::ROWS : values.size()-1;
-    for(int i=0;i<m_valid_lines;i++){
-        m_chart->series().at(i)->setName(values.at(i+1));
-        emitTagChangedSignal(i,values.at(i+1));
-    }
+    QString line;
+    QStringList values,heads;
     QDateTime momentInTime;
     LineType* p_line;
+    QList<QPointF> points[mzsview::ROWS];
+    qint64 current_x;
+    int read_flag=-1;
     while (!stream.atEnd()) {
+        line = (stream.readLine()).trimmed();
+        if(line.isEmpty())
+            continue;
+        values = line.split(mzsview::DATA_DELIMITER, QString::SkipEmptyParts);
+        if(values.size()<2)
+            continue;
+        if(read_flag==-1){
+            m_valid_lines=mzsview::ROWS<values.size()-1?mzsview::ROWS : values.size()-1;
+            heads=values;
+            read_flag=0;
+        }else if(read_flag==0){
+            momentInTime=QDateTime::fromString(values[0],mzsview::DATETIME_FORMAT);
+            if(!momentInTime.isValid())
+                continue;
+            current_x=momentInTime.toMSecsSinceEpoch();
+            read_flag=1;
+            break;
+        }
+    }
+    if(read_flag<1){
+        datafile.close();
+        return -1;
+    }
+    if(flag==0){
+        m_endtime=m_endtime-m_starttime+current_x;
+        m_starttime=current_x;
+    }
+//    qDebug()<<QDateTime::fromMSecsSinceEpoch(current_x).toString()<<":"
+//           <<QDateTime::fromMSecsSinceEpoch(m_starttime).toString()<<":"
+//          <<QDateTime::fromMSecsSinceEpoch(m_endtime).toString()<<":";
+    if(current_x>=m_endtime){
+        datafile.close();
+        return -1;
+    }
+    while(!stream.atEnd() && current_x<m_starttime){
         line = (stream.readLine()).trimmed();
         if(line.isEmpty())
             continue;
@@ -133,20 +164,80 @@ int PlotView::setAllLineData()
         momentInTime=QDateTime::fromString(values[0],mzsview::DATETIME_FORMAT);
         if(!momentInTime.isValid())
             continue;
-        for(int i=0;i<values.size()-1 && i<mzsview::ROWS;i++){
-            double y=values[i+1].toDouble();
-            if(abs(m_values_max[i]-m_values_min[i])>mzsview::ERR){
-                y=(y-m_values_min[i])/(m_values_max[i]-m_values_min[i])*100.0;
-            }
-            p_line=(LineType*)m_chart->series().at(i);
-            p_line->append(momentInTime.toMSecsSinceEpoch(),y);
+        current_x=momentInTime.toMSecsSinceEpoch();
+    }
+//    qDebug()<<QDateTime::fromMSecsSinceEpoch(current_x).toString()<<":"
+//           <<QDateTime::fromMSecsSinceEpoch(m_starttime).toString()<<":"
+//          <<QDateTime::fromMSecsSinceEpoch(m_endtime).toString()<<":";
+    if(current_x<m_starttime){
+        datafile.close();
+        return -1;
+    }
+    if(flag!=4){
+        emptyChart();//empty data for every line series
+        for(int i=0;i<m_valid_lines;i++){
+            m_chart->series().at(i)->setName(heads.at(i+1));
+            emitTagChangedSignal(i,heads.at(i+1));
         }
+        while(!stream.atEnd() && current_x<=m_endtime){
+            for(int i=0;i<values.size()-1 && i<mzsview::ROWS;i++){
+                double y=values[i+1].toDouble();
+                if(abs(m_values_max[i]-m_values_min[i])>mzsview::ERR){
+                    y=(y-m_values_min[i])/(m_values_max[i]-m_values_min[i])*100.0;
+                }
+                points[i].append(QPointF(current_x,y));
+            }
+            line = (stream.readLine()).trimmed();
+            if(line.isEmpty())
+                continue;
+            values = line.split(mzsview::DATA_DELIMITER, QString::SkipEmptyParts);
+            if(values.size()<2)
+                continue;
+            momentInTime=QDateTime::fromString(values[0],mzsview::DATETIME_FORMAT);
+            if(!momentInTime.isValid())
+                continue;
+            current_x=momentInTime.toMSecsSinceEpoch();
+        }
+        for(int i=0;i<values.size()-1 && i<mzsview::ROWS;i++){
+            p_line=(LineType*)m_chart->series().at(i);
+            p_line->replace(points[i]);
+        }
+    }else{
+        if(field_index<0 || line_No<0 || line_No>=m_chart->series().size()-2 || line_No>=mzsview::ROWS){
+            datafile.close();
+            return -1;
+        }
+        ((LineType*)m_chart->series().at(line_No))->clear();
+        m_chart->series().at(line_No)->setName(heads.at(field_index));
+        if(values.size()<2 || values.size()<=field_index){
+            datafile.close();
+            return -1;
+        }
+        while(!stream.atEnd() && current_x<=m_endtime){
+            double y=values[field_index].toDouble();
+                if(abs(m_values_max[line_No]-m_values_min[line_No])>mzsview::ERR){
+                    y=(y-m_values_min[line_No])/(m_values_max[line_No]-m_values_min[line_No])*100.0;
+                }
+            points[line_No].append(QPointF(current_x,y));
+            line = (stream.readLine()).trimmed();
+            if(line.isEmpty())
+                continue;
+            values = line.split(mzsview::DATA_DELIMITER, QString::SkipEmptyParts);
+            if(values.size()<2 || values.size()<=field_index)
+                continue;
+            momentInTime=QDateTime::fromString(values[0],mzsview::DATETIME_FORMAT);
+            if(!momentInTime.isValid())
+                continue;
+            current_x=momentInTime.toMSecsSinceEpoch();
+        }
+        p_line=(LineType*)m_chart->series().at(line_No);
+        p_line->replace(points[line_No]);
     }
     datafile.close();
     return 0;
 }
 
-int PlotView::setLineData(int line_No, int field_index)
+int PlotView::setLineData2(int line_No, int field_index)
 {
     if(field_index<0 || line_No<0 || line_No>=m_chart->series().size()-2)
         return -1;
@@ -200,31 +291,61 @@ void PlotView::setCursor(int flag)//flag 0:called by loadChart,1:called by nextT
     }
     double x_left=0.0;
     double x_right=0.0;
+    qint64 point_count;
     if(p_line!=NULL && p_line->count()>1){
         x_left=p_line->at(0).x();
-        x_right=p_line->at(p_line->count()-1).x();
+        point_count=p_line->count();
+        x_right=p_line->at(point_count-1).x();
     }else{
+        return;
+    }
+    if(abs(x_right-x_left)<mzsview::ERR){
         return;
     }
     if(flag==0){
         m_current_lefttime=x_left;
+        m_leftpoint_index=0;
+        if(m_current_lefttime+m_time_span<x_right){
+            x_right=m_current_lefttime+m_time_span;
+        }
     }else if(flag==1){
-        if(m_current_lefttime+m_time_span>x_right){
+        if(m_current_lefttime+m_time_span>x_right && m_current_lefttime+m_time_span>m_endtime){
             return;
         }
         m_current_lefttime=m_current_lefttime+m_time_span;
         x_left=m_current_lefttime;
         x_right=m_current_lefttime+m_time_span;
     }else if(flag==2){
-        if(m_current_lefttime-m_time_span<x_left){
+        if(m_current_lefttime-m_time_span<x_left && m_current_lefttime-m_time_span<m_starttime){
             return;
         }
         m_current_lefttime=m_current_lefttime-m_time_span;
-    }else if(flag!=3){
+        x_left=m_current_lefttime;
+        x_right=m_current_lefttime+m_time_span;
+    }else if(flag==3){
+        x_left=m_current_lefttime;
+        if(m_current_lefttime+m_time_span<x_right){
+            x_right=m_current_lefttime+m_time_span;
+        }
+    }else{
         return;
     }
-    x_left=m_current_lefttime;
-    x_right=m_current_lefttime+m_time_span;
+//    qint64 j=0;
+//    if(flag!=2){
+//        for(j=m_leftpoint_index;j<point_count;j++){
+//            if(p_line->at(j).x()>x_right)
+//                break;
+//        }
+//        m_rightpoint_index=j-1;
+//    }else{
+//        for(j=m_rightpoint_index;j>=0;j--){
+//           // qDebug()<<j<<":"<<QDateTime::fromMSecsSinceEpoch(p_line->at(j).x()).toString()<<":"
+//             //       <<QDateTime::fromMSecsSinceEpoch(x_left).toString();
+//            if(p_line->at(j).x()<x_left)
+//                break;
+//        }
+//        m_leftpoint_index=j+1;
+//    }
     m_cursor_left->clear();
     m_cursor_right->clear();
     m_cursor_left->append(x_left,m_minY);
@@ -238,16 +359,16 @@ void PlotView::setCursor(int flag)//flag 0:called by loadChart,1:called by nextT
     m_status->setPos(m_chart->size().width()*0.1, m_chart->size().height()*0.92);
     m_status->setText(QString("cursor1 %1   cursor2 %2").arg(stime0).arg(stime1));
     m_cursor_picked=NULL;
-    if(flag==0){
-        m_isloadchart=true;
-    }
+    //if(flag==0){
+    m_isloadchart=true;
+    //}
     emitCursorChangedSignal(mzsview::CURSOR1_COLUMN,x_left);
     emitCursorChangedSignal(mzsview::CURSOR2_COLUMN,x_right);
 }
 
 int PlotView::loadChart()
 {
-    if(setAllLineData()!=0)
+    if(setLineData(0)!=0)
         return -1;
     setCursor(0);
     m_isloadchart=true;
@@ -260,8 +381,20 @@ void PlotView::setTimespan()
     double time_span=QInputDialog::getDouble(this,tr("Input Time Span"),tr("Hours:"),2,1,24,1, &ok);
     if(ok){
         m_time_span=time_span*3600000;
-        setCursor(3);
-
+        if(m_current_lefttime+m_time_span>=m_endtime){
+            qint64 tmp_s=m_starttime;
+            qint64 tmp_e=m_endtime;
+            m_starttime=m_current_lefttime;
+            m_endtime=m_starttime+m_time_span;
+            if(setLineData(3)!=0){//failure
+                m_endtime=tmp_e;
+                m_starttime=tmp_s;
+            }else{
+                setCursor(3);
+            }
+        }else{
+            setCursor(3);
+        }
     }
 }
 
@@ -269,28 +402,57 @@ void PlotView::nextTime()
 {
     if(!m_isloadchart)
         return;
-    setCursor(1);
+    if(m_current_lefttime+m_time_span>=m_endtime){
+        qint64 tmp=m_endtime-m_starttime;
+        m_starttime=m_endtime;
+        m_endtime=m_starttime+tmp;
+        if(setLineData(1)!=0){//failure
+            m_endtime=m_starttime;
+            m_starttime=m_endtime-tmp;
+        }else{
+            setCursor(1);
+        }
+    }else{
+        setCursor(1);
+    }
 }
 
 void PlotView::prevTime()
 {
     if(!m_isloadchart)
         return;
-    setCursor(2);
+    if(m_current_lefttime-m_time_span<m_starttime){
+        qint64 tmp=m_endtime-m_starttime;
+        m_endtime=m_starttime;
+        m_starttime=m_endtime-tmp;
+        if(setLineData(2)!=0){//failure
+            m_starttime=m_endtime;
+            m_endtime=m_starttime+tmp;
+        }else{
+            setCursor(2);
+        }
+    }else{
+        setCursor(2);
+    }
 }
 
 int PlotView::findYbyX(LineType *series,int series_index,double x,double& y)//series point.x must be sorted by ascend
 {
     if(series==NULL)
         return -1;
-    int n=series->count();
+    qint64 n=series->count();
     if(n<2)
         return -1;
-    int from=0,to=n-1;
-    int i;
+    qint64 from=0;//m_leftpoint_index;
+    qint64 to=n-1;//m_rightpoint_index;
+    //from=m_leftpoint_index;
+    //to=m_rightpoint_index;
+    qint64 i;
     double xx;
+    int j=0;
+    //qDebug()<<"start:"<<from<<":"<<to<<":"<<j;
     while(from<to-1){
-        i=int((from+to)/2);
+        i=(from+to)/2;
         xx=series->at(i).x();
         if(abs(xx-x)<mzsview::ERR){
             y=series->at(i).y();
@@ -300,7 +462,9 @@ int PlotView::findYbyX(LineType *series,int series_index,double x,double& y)//se
             to=i;
         else
             from=i;
+        j++;
     }
+    //qDebug()<<"end:"<<from<<":"<<to<<":"<<j;
     double y0,y1,x0,x1;
     y0=series->at(from).y();
     y1=series->at(to).y();
@@ -334,9 +498,7 @@ void PlotView::mouseMoveEvent(QMouseEvent *event)
 {
     if(m_isloadchart && m_cursor_picked!=NULL){
         double x=m_chart->mapToValue(event->pos()).x();
-        m_cursor_picked->clear();
-        m_cursor_picked->append(x,m_minY);
-        m_cursor_picked->append(x,m_maxY);
+        m_cursor_picked->replace(QList<QPointF>{{x,1.0*m_minY},{x,1.0*m_maxY}});
         QString stime0=QDateTime::fromMSecsSinceEpoch(m_cursor_left->at(0).x()).toString("hh:mm:ss");
         QString stime1=QDateTime::fromMSecsSinceEpoch(m_cursor_right->at(0).x()).toString("hh:mm:ss");
         m_status->setPos(m_chart->size().width()*0.1, m_chart->size().height()*0.92);
@@ -346,7 +508,7 @@ void PlotView::mouseMoveEvent(QMouseEvent *event)
         else if(m_cursor_right==m_cursor_picked)
             emitCursorChangedSignal(mzsview::CURSOR2_COLUMN,x);
     }
-    QChartView::mouseMoveEvent(event);
+    //QChartView::mouseMoveEvent(event);
 }
 
 void PlotView::mouseDoubleClickEvent(QMouseEvent *event)
@@ -364,9 +526,7 @@ void PlotView::mouseDoubleClickEvent(QMouseEvent *event)
             mycursor=m_cursor_right;
             cursor_column=mzsview::CURSOR2_COLUMN;
         }
-        mycursor->clear();
-        mycursor->append(x,m_minY);
-        mycursor->append(x,m_maxY);
+        mycursor->replace(QList<QPointF>{{x,1.0*m_minY},{x,1.0*m_maxY}});
         if(m_cursor_picked!=NULL)
             m_cursor_picked=NULL;
         else
@@ -377,7 +537,7 @@ void PlotView::mouseDoubleClickEvent(QMouseEvent *event)
         m_status->setText(QString("cursor1 %1   cursor2 %2").arg(stime0).arg(stime1));
         emitCursorChangedSignal(cursor_column,x);
     }
-    QGraphicsView::mouseDoubleClickEvent(event);
+    QChartView::mouseDoubleClickEvent(event);
 }
 
 void PlotView::pickLine()
@@ -401,6 +561,9 @@ void PlotView::pickLine()
 
 void PlotView::emitCursorChangedSignal(int cursor_column,double cursorX)
 {
+
+    //QTime t;
+    //t.start();
     if(m_isloadchart){
         QList<double> y_list;
         double y;
@@ -408,8 +571,10 @@ void PlotView::emitCursorChangedSignal(int cursor_column,double cursorX)
             findYbyX((LineType *)m_chart->series().at(i),i,cursorX,y);
             y_list.append(y);
         }
+        //qDebug()<<"findYbyX time(ms):"<<t.elapsed();
         emit cursorValueChanged(cursor_column,y_list);
     }
+    //qDebug()<<"cursorchanged time(ms):"<<t.elapsed();
 }
 
 void PlotView::emitTagChangedSignal(int rowIndex, const QString &tag)
@@ -452,7 +617,7 @@ void PlotView::modelDataChanged(const int row_index, const QString &tag, int tag
         m_values_max[row_index]=max;
         m_values_min[row_index]=min;
         if(is_line_changed){//update line
-            if(setLineData(row_index,tag_field_index)==0){
+            if(setLineData(4,row_index,tag_field_index)==0){
                 double x_left=m_cursor_left->at(0).x();
                 double x_right=m_cursor_right->at(0).x();
                 emitCursorChangedSignal(mzsview::CURSOR1_COLUMN,x_left);
